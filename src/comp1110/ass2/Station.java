2651776eaa9bf57cf9bb05b1cd4830fa8d59c574
Ganaraj Rao <u7097254@anu.edu.au>
2020-03-29 14:19:19 +1100

    updated originality

--- src/comp1110/ass2/Station.java
+++ src/comp1110/ass2/Station.java
@@ -1,6 +1,7 @@
 package comp1110.ass2;
 
 public class Station {
+    //represents stations on board
     private int numberOfPlayers;
     private int playerNumber;
     private int[] stationOwned;
