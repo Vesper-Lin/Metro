ec840c9566d5dbf6e7ef453a1d11d17d08358e52
Yuxuan Lin <u6828533@anu.edu.au>
2020-04-16 12:42:44 +1000

    Task 4

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -1,5 +1,6 @@
 package comp1110.ass2.gui;
 
+import comp1110.ass2.Board;
 import comp1110.ass2.Metro;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -51,6 +52,7 @@ public class Viewer extends Application {
     }
 
     void drawBoard(){
+        board.getChildren().clear();
         for (int row = 0; row < 10; row++) {
             for (int col = 0; col < 10; col++) {
                 Rectangle r = new Rectangle(SQUARE_SIZE, SQUARE_SIZE);
@@ -60,6 +62,113 @@ public class Viewer extends Application {
                 board.getChildren().add(r);
             }
         }
+
+        for (int i=1; i<9; i++){
+            ImageView background = new ImageView();
+            background.setImage(new Image(this.getClass().getResource(URI_BASE + "station" + i + ".jpg").toString()));
+            background.setFitWidth(SQUARE_SIZE);
+            background.setFitHeight(SQUARE_SIZE);
+            background.setLayoutY(0);
+            background.setLayoutX(162+ 70*(9-i));
+            background.setRotate(180);
+            board.getChildren().add(background);
+        }
+        for (int i=9; i<17; i++){
+            ImageView background = new ImageView();
+            background.setImage(new Image(this.getClass().getResource(URI_BASE + "station" + i + ".jpg").toString()));
+            background.setFitWidth(SQUARE_SIZE);
+            background.setFitHeight(SQUARE_SIZE);
+            background.setLayoutY(70*(i-8));
+            background.setLayoutX(162);
+            background.setRotate(90);
+            board.getChildren().add(background);
+        }
+        for (int i=17; i<25; i++){
+            ImageView background = new ImageView();
+            background.setImage(new Image(this.getClass().getResource(URI_BASE + "station" + i + ".jpg").toString()));
+            background.setFitWidth(SQUARE_SIZE);
+            background.setFitHeight(SQUARE_SIZE);
+            background.setLayoutY(630);
+            background.setLayoutX(162 + 70*(i-16));
+            board.getChildren().add(background);
+        }
+        for (int i=25; i<33; i++){
+            ImageView background = new ImageView();
+            background.setImage(new Image(this.getClass().getResource(URI_BASE + "station" + i + ".jpg").toString()));
+            background.setFitWidth(SQUARE_SIZE);
+            background.setFitHeight(SQUARE_SIZE);
+            background.setLayoutY(70*(33-i));
+            background.setLayoutX(792);
+            background.setRotate(270);
+            board.getChildren().add(background);
+        }
+
+        ImageView background = new ImageView();
+        background.setImage(new Image(this.getClass().getResource(URI_BASE + "centre_station.jpg").toString()));
+        background.setFitWidth(SQUARE_SIZE);
+        background.setFitHeight(SQUARE_SIZE);
+        background.setLayoutY(280);
+        background.setLayoutX(442);
+        background.setRotate(270);
+        board.getChildren().add(background);
+
+        ImageView background2 = new ImageView();
+        background2.setImage(new Image(this.getClass().getResource(URI_BASE + "centre_station.jpg").toString()));
+        background2.setFitWidth(SQUARE_SIZE);
+        background2.setFitHeight(SQUARE_SIZE);
+        background2.setLayoutY(350);
+        background2.setLayoutX(442);
+        background2.setRotate(180);
+        board.getChildren().add(background2);
+
+        ImageView background3 = new ImageView();
+        background3.setImage(new Image(this.getClass().getResource(URI_BASE + "centre_station.jpg").toString()));
+        background3.setFitWidth(SQUARE_SIZE);
+        background3.setFitHeight(SQUARE_SIZE);
+        background3.setLayoutY(350);
+        background3.setLayoutX(512);
+        background3.setRotate(90);
+        board.getChildren().add(background3);
+
+        ImageView background4 = new ImageView();
+        background4.setImage(new Image(this.getClass().getResource(URI_BASE + "centre_station.jpg").toString()));
+        background4.setFitWidth(SQUARE_SIZE);
+        background4.setFitHeight(SQUARE_SIZE);
+        background4.setLayoutY(280);
+        background4.setLayoutX(512);
+        board.getChildren().add(background4);
+
+        ImageView background5 = new ImageView();
+        background5.setImage(new Image(this.getClass().getResource(URI_BASE + "tile_back_cover.jpg").toString()));
+        background5.setFitWidth(SQUARE_SIZE);
+        background5.setFitHeight(SQUARE_SIZE);
+        background5.setLayoutY(0);
+        background5.setLayoutX(162);
+        board.getChildren().add(background5);
+
+        ImageView background6 = new ImageView();
+        background6.setImage(new Image(this.getClass().getResource(URI_BASE + "tile_back_cover.jpg").toString()));
+        background6.setFitWidth(SQUARE_SIZE);
+        background6.setFitHeight(SQUARE_SIZE);
+        background6.setLayoutY(0);
+        background6.setLayoutX(792);
+        board.getChildren().add(background6);
+
+        ImageView background7 = new ImageView();
+        background7.setImage(new Image(this.getClass().getResource(URI_BASE + "tile_back_cover.jpg").toString()));
+        background7.setFitWidth(SQUARE_SIZE);
+        background7.setFitHeight(SQUARE_SIZE);
+        background7.setLayoutY(630);
+        background7.setLayoutX(792);
+        board.getChildren().add(background7);
+
+        ImageView background8 = new ImageView();
+        background8.setImage(new Image(this.getClass().getResource(URI_BASE + "tile_back_cover.jpg").toString()));
+        background8.setFitWidth(SQUARE_SIZE);
+        background8.setFitHeight(SQUARE_SIZE);
+        background8.setLayoutY(630);
+        background8.setLayoutX(162);
+        board.getChildren().add(background8);
     }
     /**
      * Draw a placement in the window, removing any previously drawn one
