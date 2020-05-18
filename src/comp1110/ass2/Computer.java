5ab4996b492fc43630945d83eab6e325cd6ba760
Ganaraj Rao <u7097254@anu.edu.au>
2020-05-18 17:14:55 +1000

    Placeholder PlayerInt Interface and Computer Class for further implemntation

---
+++ src/comp1110/ass2/Computer.java
@@ -0,0 +1,20 @@
+package comp1110.ass2;
+
+import java.util.ArrayList;
+
+public class Computer extends Player implements PlayerInt{
+    public Computer(int numberOfPlayer, int playerNumber) {
+        super(numberOfPlayer, playerNumber);
+    }
+
+
+
+    @Override
+    public void getPlayerStations() {
+        }
+
+    @Override
+    public void play() {
+
+    }
+}
